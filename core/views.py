bd5532c1417d0c378168f1bba7482d2072c911b6
Lian Carvalho <carvalhocosta842@gmail.com>
2025-01-11 20:16:46 -0300

    Some modifications

--- core/views.py
+++ core/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render
 from .forms import ClienteForm
+from .models import Cliente
 
 def Index(request):
     if request.method == 'POST':
         form = ClienteForm(request.POST)
         if form.is_valid():
             form.save()
+            form = ClienteForm()
     else:
         form = ClienteForm()
     return render(request, 'index.html', {'form': form})
